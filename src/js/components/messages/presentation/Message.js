c99dda8fdb2d04909d2e69f26bd0cc3fb99d5eed
Tomislav Priselac <tomislav.priselac@farmeron.com>
2017-02-08 13:57:59 +0100

    Handled auto remove message after 5 seconds

--- src/js/components/messages/presentation/Message.js
+++ src/js/components/messages/presentation/Message.js
@@ -3,18 +3,30 @@ import classnames from 'classnames';
 import * as messageTypes from '../../../enums/messageTypes';
 
 class Message extends Component {
-    onClick(){
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
         this.props.deleteMessage(this.props.message.id);
+    }
+
+    componentWillMount() {
+        setTimeout(function () {
+            this.onClick();
+        }.bind(this), 5000);
     }
 
     render() {
         const { id, type, text } = this.props.message;
+
         return (
-            <div class={classnames('alert',{
+            <div class={classnames('alert', {
                 'alert-success': type === messageTypes.SUCCESS,
                 'alert-danger': type === messageTypes.ERROR
             })}>
-            <button onClick={this.onClick.bind(this)} class="close"><span>&times;</span></button>
+                <button onClick={this.onClick} class="close"><span>&times;</span></button>
                 {text}
             </div>
         );
